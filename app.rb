88c19a441e6ee1410e3bcc1e89760c947bc33b67
Adrianna Valdivia <avaldivi@ggc.edu>
2017-04-08 14:27:09 -0400

    Added config files for envir to be set up on a local machine

--- app.rb
+++ app.rb
@@ -1,17 +1,9 @@
 require 'sinatra'
 require 'uri'
 require 'active_record'
+require './config/environments'
 
-db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///mylinks')
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
 
 class Link < ActiveRecord::Base
   has_many :comments  
@@ -27,53 +19,55 @@ class Comment < ActiveRecord::Base
 end
 
 
-get '/' do
-  @links = Link.order("id DESC")
-  erb :index
-end
 
-get "/about" do
-  erb :about
-end
- 
-get '/create' do
-  erb :create
-end
-
-get '/add_comment' do
-  erb :comment
-end
-
-get '/error' do
-  erb :error
-end
-
-
-post '/create' do
-  link = Link.new(params[:link])
-  if link.save
-    redirect to "/"
-  else
-     redirect to "/error"
+  get '/' do
+    @links = Link.order("id DESC")
+    erb :index
   end
-end
 
-post '/add_comment' do
-  comment = Comment.new(params[:comment])
-  if comment.save
-    redirect to "/"
-  else
-     redirect to "/error"
+  get "/about" do
+    erb :about
   end
-end
 
-get '/comment/:id' do
-  @link_comments = Comment.where(link_id:params[:id].to_i)
-end
+  get '/create' do
+    erb :create
+  end
 
-def refresh_comments(id)
-  Comment.where(link_id: id)
-end
+  get '/add_comment' do
+    erb :comment
+  end
+
+  get '/error' do
+    erb :error
+  end
+
+
+  post '/create' do
+    link = Link.new(params[:link])
+    if link.save
+      redirect to "/"
+    else
+       redirect to "/error"
+    end
+  end
+
+  post '/add_comment' do
+    comment = Comment.new(params[:comment])
+    if comment.save
+      redirect to "/"
+    else
+       redirect to "/error"
+    end
+  end
+
+  get '/comment/:id' do
+    @link_comments = Comment.where(link_id:params[:id].to_i)
+  end
+
+  def refresh_comments(id)
+    Comment.where(link_id: id)
+  end
+
 
 
 
